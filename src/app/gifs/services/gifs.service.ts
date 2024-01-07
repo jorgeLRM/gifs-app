08c33b95c1d7827c954853a0cec5205a1d99ffe2
jorgeLRM <jorge.lrm99@gmail.com>
2024-01-07 15:19:14 -0600

    Try API Giphy

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,18 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = [];
 
-  constructor() {}
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'bDii4iOrsfcBPmEeFTzWb86rzR8J4mNP';
+  private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -23,8 +29,19 @@ export class GifsService {
     this._tagsHistory = this._tagsHistory.splice(0, 10);
   }
 
-  searchTag(tag: string): void {
+  async searchTag(tag: string): Promise<void> {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+      });
   }
 }
